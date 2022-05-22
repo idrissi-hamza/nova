406a10bb6736859e210dc3e4f07314b3225e4270
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-05-22 13:35:53 +0200

    create project details page fetchind project

--- src/routes/project.jsx
+++ src/routes/project.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useDocument } from "../hooks/useDocument";
 
-const  Project = () => {
+const Project = () => {
+  const { id } = useParams();
+  const { error, document } = useDocument("projects", id);
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center h-full  text-red-400">
+        {error}
+      </div>
+    );
+  }
+  if (!document) {
+    return (
+      <div className="flex items-center justify-center h-full">Loading...</div>
+    );
+  }
   return (
-    <div> Project details</div>
-  )
-}
+    <div className="m-8">
+      <h1 className="text-xl font-semibold uppercase text-slate-700">{document.name}</h1>
+    </div>
+  );
+};
 
-export default  Project
+export default Project;
